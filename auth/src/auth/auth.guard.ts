eceb5adb77d76b386a31ce70fcb63878999e0c53
Peter <peterfayez1285@gmail.com>
2024-08-23 23:28:33 +0300

    Added @Public Decorator

--- auth/src/auth/auth.guard.ts
+++ auth/src/auth/auth.guard.ts
@@ -2,12 +2,24 @@ import { CanActivate, ExecutionContext, Injectable, UnauthorizedException, } fro
 import { JwtService } from '@nestjs/jwt';
 import { jwtConstants } from './constants';
 import { Request } from 'express';
+import { AuthService } from './auth.service';
+import { Reflector } from '@nestjs/core';
+import { IS_PUBLIC_KEY } from './decorators/public.decorator';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
-  constructor(private jwtService: JwtService) {}
+  constructor(private jwtService: JwtService, private reflector: Reflector) {}
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
+    const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+
+    if (isPublic) {
+      return true;
+    }
+
     const request = context.switchToHttp().getRequest();
     const token = this.extractTokenFromHeader(request);
     if (!token) {
